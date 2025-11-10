3d7f264f6d62bfeea6e2e9534ec9c36d7eb93e55
IOisLD <beefie13jerky@gmail.com>
2025-11-10 16:45:03 +0800

    Adding card layout and button if

--- main.js
+++ main.js
@@ -2,6 +2,20 @@
 const button = document.getElementById('changeColor');
 const colorCode = document.getElementById('colorCode');
 
+colorCode.style.color = 'blue';
+
+button.addEventListener('click',function(){
+  if (button.textContent === 'Iloveyou'){
+    button.textContent = 'Iloveyoutoo ❤️';
+    button.style.backgroundColor = 'dark blue';
+  } else {
+    (button.textContent === 'Iloveyoutoo');
+    button.textContent = 'Iloveyou';
+  }
+  
+}
+)
+
 // Add an event listener that runs when the button is clicked
 button.addEventListener('click', function() {
   // Generate a random color
